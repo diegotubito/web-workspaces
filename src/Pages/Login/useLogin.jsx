baca2fbcaac86aed3bf0ccd53765b2859e43ea8a
David Diego Gomez <diego@MBP-de-David.fibertel.com.ar>
2024-02-17 17:24:29 -0300

    use custom hooks for login

---
+++ src/Pages/Login/useLogin.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export const useLogin = () => {
+  const [loginError, setLoginError] = useState('');
+  const [loginSuccess, setLoginSuccess] = useState(false)
+
+  const doLogin = async (email, password) => {
+    try {
+      const response = await axios.post('http://127.0.0.1:666/api/v1/login', { email, password });
+      setLoginError('');
+      console.log('Login successful:', response.data);
+      setLoginSuccess(true)
+      setLoginError(null);
+    } catch (error) {
+      setLoginSuccess(false)
+      if (error.response) {
+        console.error(`Error ${error.response.status}:`, error.response.data);
+        setLoginError('Invalid credentials'); // Update with appropriate error message
+      } else {
+        console.error('Login error:', error.message);
+        setLoginError('An error occurred. Please try again later.');
+      }
+    }
+  };
+
+  return { doLogin, loginError, loginSuccess };
+};
--- src/Pages/Login/useLoginHook.jsx
+++
@@ -1,6 +0,0 @@
-const useLoginHook = () => {
-    
-    return {
-
-    }
-}
